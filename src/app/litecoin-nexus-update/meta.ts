068b3b1b336d0aa0f1b7677f50b62a30439ade12
cryptoguard <cryptoguard@protonmail.com>
2026-04-15 10:51:14 -0400

    April 2026 report update (Litecoin's Nexus, light wallets, upcoming release)

---
+++ src/app/litecoin-nexus-update/meta.ts
@@ -0,0 +1,13 @@
+import type { PostMetadata } from '@/lib/postTypes';
+
+export const post: PostMetadata = {
+  slug: 'litecoin-nexus-update',
+  title: "BasicSwap Integration on Litecoin's Nexus Mobile Wallet",
+  date: '2026-04-14',
+  author: 'BasicSwap Team',
+  tags: ['News', 'Release'],
+  description:
+    'BasicSwap DEX is coming to mobile! Learn more about the upcoming integration on Litecoin\'s Nexus wallet in this progress report.',
+  thumbnail: '/images/blog/dev_update_thumbnail.jpg',
+  bannerImage: '/images/blog/dev_update_banner.jpg',
+};
