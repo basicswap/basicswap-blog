fe8ad1d4184cab2d4922a834fe0c3c1abf8ab418
cryptoguard <cryptoguard@protonmail.com>
2026-08-01 10:27:27 -0400

    Added 0.17.7 blog post (update now)

---
+++ src/app/basicswap-release-v017_7/meta.ts
@@ -0,0 +1,16 @@
+import type { PostMetadata } from '@/lib/postTypes';
+
+export const post: PostMetadata = {
+  "slug": "basicswap-release-v017_7",
+  "title": "BasicSwap v0.17.7: A Critical Security Fix for Adaptor-Signature Swaps (Update Now)",
+  "date": "2026-08-01",
+  "author": "BasicSwap Team",
+  "tags": [
+    "News",
+    "Release",
+    "Security"
+  ],
+  "description": "BasicSwap v0.17.7 is a critical security fix for adaptor-signature swaps. It tightens how BasicSwap validates signatures from a swap counterparty and repairs any swap already affected. A drop-in from v0.17.6, mandatory for anyone running these swaps. Update now.",
+  "thumbnail": "/images/blog/Medium_BSX_17_7.jpg",
+  "bannerImage": "/images/blog/Blog_BSX_17_7.jpg"
+};
