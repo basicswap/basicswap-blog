d156a211311cb3696b7d13e6660cd6570cd92912
cryptoguard <cryptoguard@protonmail.com>
2026-08-02 20:16:01 -0400

    Added 0.17.9 blog post

---
+++ src/app/basicswap-release-v017_8-v017_9/meta.ts
@@ -0,0 +1,16 @@
+import type { PostMetadata } from '@/lib/postTypes';
+
+export const post: PostMetadata = {
+  "slug": "basicswap-release-v017_8-v017_9",
+  "title": "BasicSwap v0.17.8 and v0.17.9: Bitcoin Cash Swap Hardening, Shorter Swap Timeouts, and a Litecoin Core Update",
+  "date": "2026-08-02",
+  "author": "BasicSwap Team",
+  "tags": [
+    "News",
+    "Release",
+    "Security"
+  ],
+  "description": "Two BasicSwap releases in one post. v0.17.8 hardens Bitcoin Cash swaps and fixes a bug that could leave a BCH swap stuck on a non-default fee rate. v0.17.9 shortens swap timeouts by default, hardens how long a swap waits on a counterparty, and bumps the Litecoin core to v0.21.5.6, which carries important Litecoin security fixes. Update recommended, and promptly if you run Bitcoin Cash or Litecoin.",
+  "thumbnail": "/images/blog/Medium_BSX_17_9.jpg",
+  "bannerImage": "/images/blog/Blog_BSX_17_9.jpg"
+};
--- src/lib/postIndex.ts
+++ src/lib/postIndex.ts
@@ -12,6 +12,7 @@ import { post as basicswapReleaseV017_1 } from '@/app/basicswap-release-v017_1/m
 import { post as basicswapReleaseV017_2 } from '@/app/basicswap-release-v017_2/meta';
 import { post as basicswapReleaseV017_3V017_5 } from '@/app/basicswap-release-v017_3-v017_5/meta';
 import { post as basicswapReleaseV017_7 } from '@/app/basicswap-release-v017_7/meta';
+import { post as basicswapReleaseV017_8V017_9 } from '@/app/basicswap-release-v017_8-v017_9/meta';
 import { post as basicswapReleaseV017_6 } from '@/app/basicswap-release-v017_6/meta';
 import { post as devUpdateAugust2025 } from '@/app/dev-update-august2025/meta';
 import { post as devUpdateJuly2025 } from '@/app/dev-update-july2025/meta';
@@ -43,6 +44,7 @@ export const allPosts: PostMetadata[] = [
   basicswapReleaseV017_2,
   basicswapReleaseV017_3V017_5,
   basicswapReleaseV017_7,
+  basicswapReleaseV017_8V017_9,
   basicswapReleaseV017_6,
   devUpdateAugust2025,
   devUpdateJuly2025,
